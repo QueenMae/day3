8d522363ab8a5679ac5f3adc1db03503bf8210a7
QueenMae <maena.brice@epitech.digital>
2024-10-02 14:03:13 +0200

    ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,14 +1,13 @@
-function findHousing(housingToFind,hounsingList) {
-let i = 0  
-            
- while(i < housingList.length ) {
-if ( housingList [i] === housingToFind ){
- return [i,housingList[i]];
-}
-i++;
-}
-   return;null
+function findHousing(housingToFind, housingList) 
+{
+  let i = 0
 
+  while (i < housingList.length) {
+    if (housingList[i].toUpperCase() === housingToFind.toUpperCase()) {
+      return [i, housingList[i]]
+    }
+    i++
+  }
+  return
+  null
 }
-//Use console.log to debug you're code ! It is very useful
-console.log("I'm debugging")
