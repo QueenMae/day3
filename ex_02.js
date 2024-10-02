8ec7fac04c09035ed11c8c67a44fd61ec28afe53
QueenMae <maena.brice@epitech.digital>
2024-10-02 12:17:59 +0200

    ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,14 @@
+function findHousing(housingToFind,hounsingList) {
+let i = 0  
+            
+ while(i < housingList.length ) {
+if ( housingList [i] === housingToFind ){
+ return [i,housingList[i]];
+}
+i++;
+}
+   return;null
 
+}
+//Use console.log to debug you're code ! It is very useful
+console.log("I'm debugging")
